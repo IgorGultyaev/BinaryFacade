6b3dad662b97b809b140d9581f592585a42cf42b
IgorGultyaev <igor.gultyaev@mail.ru>
2022-05-19 22:55:20 +0300

    reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,17 @@ public class Main {
         BinOps bins = new BinOps(); //s
         //демонстрация
         System.out.println("a + b = c");
-        String a = "01011",b = "01011";
+        String a = "01011", b = "01011";
         System.out.println("a = " + a + " - двоичная");
         System.out.println("b = " + b + " - двоичная");
         System.out.println(Integer.parseInt(a, 2) + " + " + Integer.parseInt(b, 2));
-        String c = bins.sum(a,b);
+        String c = bins.sum(a, b);
         System.out.println(c + " - двоичная");
         System.out.println("с = " + Integer.parseInt(c, 2));
         System.out.println();
         System.out.println("a * b = c");
         System.out.println(Integer.parseInt(a, 2) + " * " + Integer.parseInt(b, 2));
-        c = bins.mult(a,b);
+        c = bins.mult(a, b);
         System.out.println(c + " - двоичная");
         System.out.println("с = " + Integer.parseInt(c, 2));
 
